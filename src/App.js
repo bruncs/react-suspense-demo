db9ef73e103213fcef653d12618aaad61c2cea90
Bruno Correia <brunocs@msn.com>
2018-12-11 23:57:29 -0200

    Add sample data file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment, Suspense } from "react";
 import { createResource } from "simple-cache-provider";
 import { withCache } from "./components/withCache";
+import sample from "./sample";
 
 const sleep = ms => new Promise(r => setTimeout(() => r(), ms));
 
@@ -10,31 +11,64 @@ const readShows = createResource(async function fetchNews() {
   return await res.json();
 });
 
-const Movies = withCache((props) => {
-  console.log(props);
+const Movies = withCache(props => {
+  const result = sample;
   return (
-  <Fragment>
-    <div className="column is-4">
-      <div className="movie">
-        <div className="movie__left">
-          <img src />
-        </div>
-        <div className="movie__right">
-          <div className="movie__right__title">Name: </div>
-          <div className="movie__right__subtitle">Score: </div>
-          <div className="movie__right__subtitle">Status: </div>
-          <div className="movie__right__subtitle">Network: </div>
-          <a href target="_blank" className="movie__right__subtitle">Link</a>
-        </div>
-      </div>
-    </div>
-  </Fragment>
-)}
-);
+    <Fragment>
+      {result &&
+        result.length &&
+        result.map(item => (
+          <div key={Math.random()} className="column is-4">
+            <div className="movie">
+              <div className="movie__left">
+                <img alt="" src={item.show.image.original} />
+              </div>
+              <div className="movie__right">
+                <div className="movie__right__title">{item.show.name}</div>
+                <div className="movie__right__subtitle">
+                  Score: {item.show.rating.average}
+                </div>
+                <div className="movie__right__subtitle">
+                  Status: {item.show.status}
+                </div>
+                <div className="movie__right__subtitle">
+                  Network: {item.show.network ? item.show.network.name : "N/A"}
+                </div>
+                <a
+                  href={item.show.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="movie__right__subtitle"
+                >
+                  Link
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
+    </Fragment>
+  );
+});
 
 class App extends Component {
   render() {
-    return <div className="App">Hello World!</div>;
+    return (
+      <Fragment>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">React Suspense Demo</h1>
+          </header>
+
+          <div className="container">
+            <div className="columns is-multiline">
+              <Suspense ms={5000} fallback={<h2>Movies list is loading...</h2>}>
+                <Movies />
+              </Suspense>
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    );
   }
 }
 
---
+++ src/sample.js
@@ -0,0 +1,394 @@
+module.exports = [
+  {
+    score: 29.366817,
+    show: {
+      id: 172,
+      url: "http://www.tvmaze.com/shows/172/suits",
+      name: "Suits",
+      type: "Scripted",
+      language: "English",
+      genres: ["Drama", "Legal"],
+      status: "Running",
+      runtime: 60,
+      premiered: "2011-06-23",
+      officialSite: "http://www.usanetwork.com/suits",
+      schedule: { time: "21:00", days: ["Wednesday"] },
+      rating: { average: 8.4 },
+      weight: 98,
+      network: {
+        id: 30,
+        name: "USA Network",
+        country: {
+          name: "United States",
+          code: "US",
+          timezone: "America/New_York"
+        }
+      },
+      webChannel: null,
+      externals: { tvrage: 27518, thetvdb: 247808, imdb: "tt1632701" },
+      image: {
+        medium:
+          "http://static.tvmaze.com/uploads/images/medium_portrait/160/402351.jpg",
+        original:
+          "http://static.tvmaze.com/uploads/images/original_untouched/160/402351.jpg"
+      },
+      summary:
+        "<p><b>Suits</b> delves into the fast-paced, high-stakes world of a top Manhattan corporate law firm where hotshot associate Harvey Specter makes a risky move by hiring Mike Ross a brilliant but unmotivated college dropout, as his associate. As he becomes enmeshed in this unfamiliar world, Mike relies heavily on the firm's best paralegal Rachel Zane and Harvey's no-nonsense assistant Donna Paulsen to help him serve justice. With a photographic memory and the street smarts of a hustler, Mike proves to be a legal prodigy despite the absence of bonafide legal credentials.</p>",
+      updated: 1542825657,
+      _links: {
+        self: { href: "http://api.tvmaze.com/shows/172" },
+        previousepisode: { href: "http://api.tvmaze.com/episodes/1521832" },
+        nextepisode: { href: "http://api.tvmaze.com/episodes/1568454" }
+      }
+    }
+  },
+  {
+    score: 27.995096,
+    show: {
+      id: 32271,
+      url: "http://www.tvmaze.com/shows/32271/suits",
+      name: "Suits",
+      type: "Scripted",
+      language: "Korean",
+      genres: ["Drama", "Legal"],
+      status: "Ended",
+      runtime: 70,
+      premiered: "2018-04-25",
+      officialSite: "http://www.kbs.co.kr/drama/suits/index.html",
+      schedule: { time: "22:00", days: ["Wednesday", "Thursday"] },
+      rating: { average: null },
+      weight: 59,
+      network: {
+        id: 128,
+        name: "KBS2",
+        country: {
+          name: "Korea, Republic of",
+          code: "KR",
+          timezone: "Asia/Seoul"
+        }
+      },
+      webChannel: null,
+      externals: { tvrage: null, thetvdb: 336811, imdb: "tt8084058" },
+      image: {
+        medium:
+          "http://static.tvmaze.com/uploads/images/medium_portrait/151/378676.jpg",
+        original:
+          "http://static.tvmaze.com/uploads/images/original_untouched/151/378676.jpg"
+      },
+      summary:
+        "<p>Choi Kyung Seo is a legendary lawyer at the top law firm in South Korea. He has charisma and an attractive appearance. He hires Go Yun Woo as a rookie lawyer for the law firm. Go Yun Woo has excellent memory.</p>",
+      updated: 1529842088,
+      _links: {
+        self: { href: "http://api.tvmaze.com/shows/32271" },
+        previousepisode: { href: "http://api.tvmaze.com/episodes/1460313" }
+      }
+    }
+  },
+  {
+    score: 2.5218608,
+    show: {
+      id: 26204,
+      url: "http://www.tvmaze.com/shows/26204/suite-precure",
+      name: "Suite Precure",
+      type: "Animation",
+      language: "Japanese",
+      genres: ["Anime", "Music"],
+      status: "Ended",
+      runtime: 25,
+      premiered: "2011-02-06",
+      officialSite: "http://asahi.co.jp/precure/",
+      schedule: { time: "08:30", days: ["Sunday"] },
+      rating: { average: null },
+      weight: 0,
+      network: {
+        id: 263,
+        name: "TV Asahi",
+        country: { name: "Japan", code: "JP", timezone: "Asia/Tokyo" }
+      },
+      webChannel: null,
+      externals: { tvrage: null, thetvdb: 248781, imdb: "tt2230587" },
+      image: {
+        medium:
+          "http://static.tvmaze.com/uploads/images/medium_portrait/103/258046.jpg",
+        original:
+          "http://static.tvmaze.com/uploads/images/original_untouched/103/258046.jpg"
+      },
+      summary:
+        "<p>Hibiki and Kanade are two friends who grew up together in Kanon Town. Very different in their personalities, they share one thing in common: A connection to music. No matter how different they are or how much their differences cause them to quarrel, they both continue to share loving hearts. One day they meet Hummy, who is sent to the Human World from Major Land to collect the scattered notes of the Melody of Happiness. They have to instantly transform into Pretty Cures to resist the threats and evil music from Mephisto.</p>",
+      updated: 1508251415,
+      _links: {
+        self: { href: "http://api.tvmaze.com/shows/26204" },
+        previousepisode: { href: "http://api.tvmaze.com/episodes/1104594" }
+      }
+    }
+  },
+  {
+    score: 2.4607856,
+    show: {
+      id: 22509,
+      url: "http://www.tvmaze.com/shows/22509/the-last-chapter",
+      name: "The Last Chapter",
+      type: "Scripted",
+      language: "French",
+      genres: ["Action", "Thriller"],
+      status: "Ended",
+      runtime: 60,
+      premiered: "2002-03-06",
+      officialSite: null,
+      schedule: { time: "21:00", days: ["Wednesday"] },
+      rating: { average: null },
+      weight: 0,
+      network: {
+        id: 451,
+        name: "ICI Radio-Canada Télé",
+        country: { name: "Canada", code: "CA", timezone: "America/Halifax" }
+      },
+      webChannel: null,
+      externals: { tvrage: null, thetvdb: 133961, imdb: "tt0299283" },
+      image: {
+        medium:
+          "http://static.tvmaze.com/uploads/images/medium_portrait/83/207710.jpg",
+        original:
+          "http://static.tvmaze.com/uploads/images/original_untouched/83/207710.jpg"
+      },
+      summary:
+        "<p>This show is about an outlaw biker group, The Triple Sixers. Characterized as one of the largest outlaw biker gangs in the world, The Triple Sixers dominate Canada. Their unrivaled presence and strength stretches from Halifax to Vancouver, with one important exception - the Sixers have no chapters in Ontario, the richest market in the country. Loosely based on the events surrounding the Hells Angels biker war in Quebec and their expansion into Ontario.</p>",
+      updated: 1507246425,
+      _links: {
+        self: { href: "http://api.tvmaze.com/shows/22509" },
+        previousepisode: { href: "http://api.tvmaze.com/episodes/978735" }
+      }
+    }
+  },
+  {
+    score: 2.4467802,
+    show: {
+      id: 38721,
+      url: "http://www.tvmaze.com/shows/38721/je-suis-trans",
+      name: "Je suis trans",
+      type: "Documentary",
+      language: "French",
+      genres: [],
+      status: "Running",
+      runtime: null,
+      premiered: "2016-01-04",
+      officialSite: null,
+      schedule: { time: "", days: [] },
+      rating: { average: null },
+      weight: 80,
+      network: {
+        id: 964,
+        name: "MOI&cie",
+        country: { name: "Canada", code: "CA", timezone: "America/Halifax" }
+      },
+      webChannel: null,
+      externals: { tvrage: null, thetvdb: 305736, imdb: null },
+      image: {
+        medium:
+          "http://static.tvmaze.com/uploads/images/medium_portrait/168/420097.jpg",
+        original:
+          "http://static.tvmaze.com/uploads/images/original_untouched/168/420097.jpg"
+      },
+      summary: null,
+      updated: 1537540894,
+      _links: {
+        self: { href: "http://api.tvmaze.com/shows/38721" },
+        previousepisode: { href: "http://api.tvmaze.com/episodes/1534043" }
+      }
+    }
+  },
+  {
+    score: 2.395229,
+    show: {
+      id: 15890,
+      url: "http://www.tvmaze.com/shows/15890/mikagura-school-suite",
+      name: "Mikagura School Suite",
+      type: "Animation",
+      language: "Japanese",
+      genres: ["Adventure", "Anime"],
+      status: "Ended",
+      runtime: null,
+      premiered: "2015-04-07",
+      officialSite: "http://mikagura-gakuen.com/",
+      schedule: { time: "", days: [] },
+      rating: { average: null },
+      weight: 0,
+      network: {
+        id: 475,
+        name: "TV Aichi",
+        country: { name: "Japan", code: "JP", timezone: "Asia/Tokyo" }
+      },
+      webChannel: null,
+      externals: { tvrage: 48081, thetvdb: 292926, imdb: null },
+      image: {
+        medium:
+          "http://static.tvmaze.com/uploads/images/medium_portrait/53/133650.jpg",
+        original:
+          "http://static.tvmaze.com/uploads/images/original_untouched/53/133650.jpg"
+      },
+      summary:
+        "<p>From the studio behind <i>Laughing Under the Clouds</i> and <i>Yuruyuri</i> comes a high school fantasy unlike any other! Like most awkward teens, formerly reclusive gamer Eruna Ichinomiya struggle to find her place at Mikagura High School. But her experience is far from ordinary, as the mischief-making members of the school's manga, calligraphy, drama, and other cultural clubs vie for total school domination in a rather extreme way—ULTIMATE ATTACKS. Get ready for the high-energy high school adventure you've only ever dreamed of.</p>",
+      updated: 1518184113,
+      _links: {
+        self: { href: "http://api.tvmaze.com/shows/15890" },
+        previousepisode: { href: "http://api.tvmaze.com/episodes/723810" }
+      }
+    }
+  },
+  {
+    score: 2.3382611,
+    show: {
+      id: 24254,
+      url: "http://www.tvmaze.com/shows/24254/les-nuits-revolutionnaires",
+      name: "Les Nuits Révolutionnaires",
+      type: "Scripted",
+      language: "French",
+      genres: ["Drama", "Action"],
+      status: "Ended",
+      runtime: 60,
+      premiered: "1989-10-05",
+      officialSite: null,
+      schedule: { time: "20:00", days: ["Monday"] },
+      rating: { average: null },
+      weight: 20,
+      network: {
+        id: 249,
+        name: "France 3",
+        country: { name: "France", code: "FR", timezone: "Europe/Paris" }
+      },
+      webChannel: null,
+      externals: { tvrage: null, thetvdb: 321453, imdb: "tt0257976" },
+      image: {
+        medium:
+          "http://static.tvmaze.com/uploads/images/medium_portrait/92/230499.jpg",
+        original:
+          "http://static.tvmaze.com/uploads/images/original_untouched/92/230499.jpg"
+      },
+      summary:
+        "<p>The eighteenth-century French writer Nicolas Restif de la Bretonne recorded history, but so embroidered with his own imagination that the distinction between fact and fiction is often blurred. He was nicknamed 'The Owl', because it was his custom to walk the streets of Paris at night. <b>Les Nuits Révolutionnaires</b> (based on Les Nuits de Paris) spans the period 1789 - 1793 and, rather than giving a factual account of those years, Restif's writing brings to life the nocturnal world of the troubled city, as he observed it during the hours of darkness. He conjures up the other characters who also haunted the shadows - amongst them The Procuress, The Blind Man and The Lamplighter. Les Nuits Révolutionnaires immerses the viewer in Restif's strange existence, away from the loud mobs, in the troubled backwaters of the Revolution. To this turbulent arena is added the drama of his own life and the inspired wit of his fantasies. Les Nuits Révolutionnaires begins a few nights before the fall of the Bastille on 14 July 1789 and covers the King's execution and the start of the Reign of Terror, ending in June 1793, but the events of the Revolution form only a background to a dramatisation of Restif's fantastic stories.</p>",
+      updated: 1483765668,
+      _links: {
+        self: { href: "http://api.tvmaze.com/shows/24254" },
+        previousepisode: { href: "http://api.tvmaze.com/episodes/1033907" }
+      }
+    }
+  },
+  {
+    score: 2.2855816,
+    show: {
+      id: 915,
+      url: "http://www.tvmaze.com/shows/915/the-suite-life-of-zack-and-cody",
+      name: "The Suite Life of Zack and Cody",
+      type: "Scripted",
+      language: "English",
+      genres: ["Comedy", "Family"],
+      status: "Ended",
+      runtime: 25,
+      premiered: "2005-03-18",
+      officialSite: null,
+      schedule: { time: "12:00", days: [] },
+      rating: { average: null },
+      weight: 66,
+      network: {
+        id: 78,
+        name: "Disney Channel",
+        country: {
+          name: "United States",
+          code: "US",
+          timezone: "America/New_York"
+        }
+      },
+      webChannel: null,
+      externals: { tvrage: 6221, thetvdb: 79316, imdb: "tt0426371" },
+      image: {
+        medium:
+          "http://static.tvmaze.com/uploads/images/medium_portrait/6/16051.jpg",
+        original:
+          "http://static.tvmaze.com/uploads/images/original_untouched/6/16051.jpg"
+      },
+      summary:
+        "<p>Meet <i>Zack and Cody</i>, 11 year-old identical twins and the newest residents of Boston's swanky Tipton Hotel on <b>The Suite Life of Zack and Cody</b>.</p>",
+      updated: 1520872959,
+      _links: {
+        self: { href: "http://api.tvmaze.com/shows/915" },
+        previousepisode: { href: "http://api.tvmaze.com/episodes/86657" }
+      }
+    }
+  },
+  {
+    score: 1.9567894,
+    show: {
+      id: 5365,
+      url: "http://www.tvmaze.com/shows/5365/mobile-suit-gundam-seed",
+      name: "Mobile Suit Gundam SEED",
+      type: "Animation",
+      language: "Japanese",
+      genres: ["Anime", "Fantasy", "Romance", "Science-Fiction"],
+      status: "Ended",
+      runtime: 30,
+      premiered: "2002-10-05",
+      officialSite: "http://www.gundam-seed.net/",
+      schedule: { time: "18:00", days: ["Saturday"] },
+      rating: { average: null },
+      weight: 0,
+      network: {
+        id: 159,
+        name: "TBS",
+        country: { name: "Japan", code: "JP", timezone: "Asia/Tokyo" }
+      },
+      webChannel: null,
+      externals: { tvrage: null, thetvdb: 74474, imdb: "tt0374407" },
+      image: {
+        medium:
+          "http://static.tvmaze.com/uploads/images/medium_portrait/22/55452.jpg",
+        original:
+          "http://static.tvmaze.com/uploads/images/original_untouched/22/55452.jpg"
+      },
+      summary:
+        "<p>C.E. 71: In the midst of war between the Naturals (OMNI) and Coordinators (ZAFT), a unit from ZAFT is dispatched to hijack the Earth Alliance's newly developed mobile suits on the neutral colony of Heliopolis. Orb Civilian Coordinator Kira Yamato attends the technical college on Heliopolis. After ZAFT hijacks 4 of the 5 mobile suits, Kira stumbles upon the last one, Strike, forced to pilot it to save his and his friend's lives. During this confusion, Kira also reunites with his childhood Coordinator friend, Athrun Zala, who ironically turns out to be a ZAFT soldier and one of the hijackers at Heliopolis. Having control of Strike, Kira joins the Earth Alliance boarding the ship known as Archangel, to protect his friends while despairing over becoming the enemy of his childhood friend and people.</p>",
+      updated: 1452371029,
+      _links: {
+        self: { href: "http://api.tvmaze.com/shows/5365" },
+        previousepisode: { href: "http://api.tvmaze.com/episodes/324799" }
+      }
+    }
+  },
+  {
+    score: 1.9545294,
+    show: {
+      id: 5246,
+      url: "http://www.tvmaze.com/shows/5246/mobile-suit-victory-gundam",
+      name: "Mobile Suit Victory Gundam",
+      type: "Animation",
+      language: "Japanese",
+      genres: ["Drama", "Action", "Anime", "Science-Fiction"],
+      status: "Ended",
+      runtime: 25,
+      premiered: "1993-04-02",
+      officialSite: "http://www.v-gundam.net/",
+      schedule: { time: "", days: ["Friday"] },
+      rating: { average: null },
+      weight: 0,
+      network: {
+        id: 263,
+        name: "TV Asahi",
+        country: { name: "Japan", code: "JP", timezone: "Asia/Tokyo" }
+      },
+      webChannel: null,
+      externals: { tvrage: null, thetvdb: 71536, imdb: "tt0159185" },
+      image: {
+        medium:
+          "http://static.tvmaze.com/uploads/images/medium_portrait/21/54940.jpg",
+        original:
+          "http://static.tvmaze.com/uploads/images/original_untouched/21/54940.jpg"
+      },
+      summary:
+        "<p>It is Universal Century 0153. On the space colonies located at Side 2, the Zanscare Empire has come to power and it holds onto that power through liberal use of the guillotine. With its forces invading the Earth and its space fleets preparing to subjugate the other Sides, the Zanscare Empire has nothing to fear from the weakening Earth Federation. It is opposed only by the League Militaire, whose state of the art mobile suits and young pilots comprise the only real resistance movement. Now, thirteen-year-old Uso Evin has been dragged into the war by a battle near his home in Eastern Europe. As the Newtype pilot of the Victory Gundam, he fights not only to defeat the Zanscare Empire but also to find out what has become of his parents, who left him behind when they went into space.</p>",
+      updated: 1520479211,
+      _links: {
+        self: { href: "http://api.tvmaze.com/shows/5246" },
+        previousepisode: { href: "http://api.tvmaze.com/episodes/320509" }
+      }
+    }
+  }
+];
